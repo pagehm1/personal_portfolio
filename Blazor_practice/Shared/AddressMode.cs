b35166ca282e6a41d18ab2df98372fe81bcc2d16
pagehm1 <hunterpage27171@gmail.com>
2023-08-19 01:50:10 -0400

    continued work on instruction set page

---
+++ Blazor_practice/Shared/AddressMode.cs
@@ -0,0 +1,18 @@
+﻿namespace Hunter_Page.Shared
+{
+    public class AddressMode
+    {
+        public string Name { get; set; }
+
+        public string Bits { get; set; }
+
+        public string Description { get; set; }
+
+        public AddressMode() 
+        { 
+            Name = string.Empty;
+            Bits = string.Empty;    
+            Description = string.Empty;
+        }
+    }
+}
