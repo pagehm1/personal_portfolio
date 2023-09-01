c462567cace5e53348ac25f088eb96619eb6857c
pagehm1 <hunterpage27171@gmail.com>
2023-09-01 00:20:55 -0400

    Added http client factory. Added polly to implement retries and timeouts for github api calls Added secondary view if github view is not available

--- Blazor_practice/Program.cs
+++ Blazor_practice/Program.cs
@@ -2,6 +2,7 @@ using Hunter_Page;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Polly;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddHttpClient("github_api", client => {
+    client.BaseAddress = new Uri("https://api.github.com");
+    client.Timeout = TimeSpan.FromSeconds(30);
+}).AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(10, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))));
 
 await builder.Build().RunAsync();
